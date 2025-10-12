7feff0228186364a67569fb24bbc336e2d83e97f
sunjaewow <chltjswo789@naver.com>
2025-10-12 12:57:46 +0900

    fix : MessageType 주석 수정

--- src/main/java/com/example/PetApp/domain/chatting/model/type/MessageType.java
+++ src/main/java/com/example/PetApp/domain/chatting/model/type/MessageType.java
@@ -1,5 +1,6 @@
 package com.example.PetApp.domain.chatting.model.type;
 
 public enum MessageType {
-    ENTER, TALK, LEAVE, READ, CHAT_UPDATE, LIST_UPDATE
+    ENTER, TALK, LEAVE, READ,//클라이언트
+    CHAT_UPDATE, LIST_UPDATE//서버 응답
 }
