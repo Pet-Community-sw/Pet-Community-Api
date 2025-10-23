337ef10090546f9c12844f807222ab0ab37cc3f0
sunjaewow <chltjswo789@naver.com>
2025-10-23 23:38:30 +0900

    info : main의 SpringApplication

--- src/main/java/com/example/petapp/PetAppApplication.java
+++ src/main/java/com/example/petapp/PetAppApplication.java
@@ -11,8 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EntityScan(basePackages = "com.example.petapp")//이걸 해야지만 전체가 컴포넌트 스캔을 함 이거 이유를 좀 찾고싶음.
 public class PetAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PetAppApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PetAppApplication.class, args);//내부에서 컨테이너 자동 생성
+    }
 }
