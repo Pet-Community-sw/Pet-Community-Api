a2e534972d6458a6cc53c59b4a47cb6c23a6b602
sunjaewow <chltjswo789@naver.com>
2025-12-20 13:19:30 +0900

    fix

--- src/main/java/com/example/petapp/application/service/role/RoleQueryService.java
+++ src/main/java/com/example/petapp/application/service/role/RoleQueryService.java
@@ -6,7 +6,9 @@ import com.example.petapp.domain.role.RoleRepository;
 import com.example.petapp.interfaces.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional(readOnly = true)
 @Service
 @RequiredArgsConstructor
 public class RoleQueryService implements RoleQueryUseCase {
@@ -17,5 +19,5 @@ public class RoleQueryService implements RoleQueryUseCase {
     public Role findOrThrow() {
         return repository.find("ROLE_USER").orElseThrow(() -> new NotFoundException("해당 role은 없습니다."));
     }
-    
+
 }
