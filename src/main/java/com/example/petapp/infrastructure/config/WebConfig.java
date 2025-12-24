755490f7fa4884010cf7f279c829d389a794efdb
sunjaewow <chltjswo789@naver.com>
2025-12-24 22:28:54 +0900

    fix :yml

--- src/main/java/com/example/petapp/infrastructure/config/WebConfig.java
+++ src/main/java/com/example/petapp/infrastructure/config/WebConfig.java
@@ -1,29 +1,12 @@
 package com.example.petapp.infrastructure.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-    @Value("${spring.file.base-path}")
-    private String basePath;
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/image/profiles/**")
-                .addResourceLocations("file:" + basePath + "/profiles/");
-        registry.addResourceHandler("/image/posts/**")
-                .addResourceLocations("file:" + basePath + "/posts/");
-        registry.addResourceHandler("/image/members/**")
-                .addResourceLocations("file:" + basePath + "/members/");
-        registry.addResourceHandler("/image/basic/**")
-                .addResourceLocations("file:" + basePath + "/basic/");
-    }
-
+    
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 API
