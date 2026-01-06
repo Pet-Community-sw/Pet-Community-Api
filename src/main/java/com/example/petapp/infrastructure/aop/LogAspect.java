0c8ac5fb81e9ab6f228daab7521188d3c9988d5f
sunjaewow <chltjswo789@naver.com>
2026-01-06 22:57:18 +0900

    fix : 포인트컷 수정

--- src/main/java/com/example/petapp/infrastructure/aop/LogAspect.java
+++ src/main/java/com/example/petapp/infrastructure/aop/LogAspect.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Slf4j//todo : aop적용안되는중 봐야함.
 public class LogAspect {
-    @Around("execution(* com.example.petapp.application.service.*Service.*(..))")
+    @Around("execution(* com.example.petapp.application.service..*Service.*(..))")
     public Object logTrace(ProceedingJoinPoint joinPoint) throws Throwable {
         log.info("[LOG] Method : {}", joinPoint.getSignature().toShortString());
         Object[] args = joinPoint.getArgs();
