dba057fe51fb89cf6d308d889c677d5c1227ee42
sunjaewow <chltjswo789@naver.com>
2026-01-20 12:16:48 +0900

    재시도 및 recover

--- src/main/java/com/example/petapp/infrastructure/event/member/MemberSearchListener.java
+++ src/main/java/com/example/petapp/infrastructure/event/member/MemberSearchListener.java
@@ -4,6 +4,10 @@ import com.example.petapp.application.in.member.object.MemberSearchEvent;
 import com.example.petapp.domain.member.MemberSearchRepository;
 import com.example.petapp.domain.member.model.MemberSearch;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
@@ -11,11 +15,16 @@ import org.springframework.transaction.event.TransactionalEventListener;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class MemberSearchListener {
 
     private final MemberSearchRepository repository;
 
     @Async("elasticExecutor")
+    @Retryable(
+            maxAttempts = 4,// 최대 재시도 횟수(기본값 3)
+            backoff = @Backoff(delay = 2000, multiplier = 2.0, random = true)// 재시도 간격
+    )
     //DB 커밋 이후에 이벤트 처리
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handle(MemberSearchEvent event) {
@@ -25,5 +34,10 @@ public class MemberSearchListener {
                 .memberImageUrl(event.memberImageUrl())
                 .build();
         repository.save(document);
+    }
+
+    @Recover
+    public void recover(Exception e, MemberSearchEvent event) {
+        log.error("ElasticSearch 저장 실패 memberId: {}, error: {}", event.memberId(), e.getMessage());
     }
 }
