4db8107f1354a82d0ed0a1e8e41a8f30e0e69cd3
sunjaewow <chltjswo789@naver.com>
2026-01-25 18:14:13 +0900

    page가 0일 때와 검색 개수가 3개 이하일 때 영어로 검색했을 때 오타교정 추가

--- src/main/java/com/example/petapp/infrastructure/database/elasticsearch/ElasticMemberSearchAdapter.java
+++ src/main/java/com/example/petapp/infrastructure/database/elasticsearch/ElasticMemberSearchAdapter.java
@@ -4,7 +4,9 @@ import com.example.petapp.application.in.member.object.dto.response.MemberSearch
 import com.example.petapp.application.out.MemberSearchPort;
 import com.example.petapp.domain.member.model.MemberSearch;
 import lombok.RequiredArgsConstructor;
+import org.elasticsearch.common.unit.Fuzziness;
 import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.SearchHit;
@@ -24,6 +26,15 @@ public class ElasticMemberSearchAdapter implements MemberSearchPort {
     //Spring Data Elasticsearch가 제공하는 Es에 검색 요청 보내는 객체
     private final ElasticsearchOperations operations;
 
+    @NotNull
+    private static NativeSearchQuery buildQuery(int page, BoolQueryBuilder queryBuilder) {
+        NativeSearchQuery query = new NativeSearchQueryBuilder()
+                .withQuery(queryBuilder)
+                .withPageable(PageRequest.of(page, 10))
+                .build();
+        return query;
+    }
+
     /**
      * 검색창에서 입력값이 바뀔 때마다 자동완성 목적.
      */
@@ -34,10 +45,7 @@ public class ElasticMemberSearchAdapter implements MemberSearchPort {
                 .should(termQuery("memberName", keyword).boost(10f))
                 .should(matchQuery("memberName.prefix", keyword).boost(6f));// matchQuery는 분석을 거친 뒤 매칭
 
-        NativeSearchQuery query = new NativeSearchQueryBuilder()
-                .withQuery(queryBuilder)
-                .withPageable(PageRequest.of(0, 10))
-                .build();
+        NativeSearchQuery query = buildQuery(0, queryBuilder);
 
         SearchHits<MemberSearch> hits = operations.search(query, MemberSearch.class);
 
@@ -54,19 +62,36 @@ public class ElasticMemberSearchAdapter implements MemberSearchPort {
      */
     @Override
     public List<MemberSearchResponseDto> search(String keyword, int page) {
-        if (keyword.length() < 2) return List.of();
-
         BoolQueryBuilder queryBuilder = boolQuery()
                 .should(termQuery("memberName", keyword).boost(10f))//keyword는 분석을 안거치므로 termQuery
-                .should(matchQuery("memberName.prefix", keyword).boost(6f))//앞부분 매칭
-                .should(matchQuery("memberName.contains", keyword).boost(2f));//중간 포함
+                .should(matchQuery("memberName.prefix", keyword).boost(6f));//앞부분 매칭
 
-        NativeSearchQuery query = new NativeSearchQueryBuilder()
-                .withQuery(queryBuilder)
-                .withPageable(PageRequest.of(page, 10))
-                .build();
 
-        SearchHits<MemberSearch> hits = operations.search(query, MemberSearch.class);
+        /**
+         * 키워드가 2글자 이상일 경우에만 중간 포함 매칭
+         */
+        if (keyword.length() > 1) {
+            queryBuilder.should(matchQuery("memberName.contains", keyword).boost(2f));//중간 포함
+        }
+
+        SearchHits<MemberSearch> hits = operations.search(buildQuery(page, queryBuilder), MemberSearch.class);
+
+        /**
+         * 키워드가 영문일 경우에만 오타 교정
+         * 0페이지에서 오타교정 (사용자는 오타인지 첫 페이지에서 확인할 수 있기 때문)
+         * 검색 결과가 3개 이하일 때만 오타교정 (결과가 많으면 오타가 아닐 확률이 높기 때문)
+         */
+        if (page == 0 && hits.getTotalHits() <= 3 && keyword.matches("^[A-Za-z]+$")) {
+            queryBuilder.should(matchQuery("memberName.fuzzy", keyword)
+                    .fuzziness(Fuzziness.AUTO)
+                    .prefixLength(1)
+                    .maxExpansions(10) //후보군 최대 개수
+                    .boost(1f)
+            );
+
+            hits = operations.search(buildQuery(page, queryBuilder), MemberSearch.class);
+        }
+
 
         return hits.getSearchHits().stream()
                 .map(this::toObject)
