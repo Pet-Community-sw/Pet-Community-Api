c1506e0a84407e1cd23e4cbc1952d4662d5a0567
sunjaewow <chltjswo789@naver.com>
2026-02-06 19:05:05 +0900

    메시지 재시도는 콜백함수 무시

--- src/main/java/com/example/petapp/infrastructure/mq/RabbitConfig.java
+++ src/main/java/com/example/petapp/infrastructure/mq/RabbitConfig.java
@@ -26,6 +26,8 @@ public class RabbitConfig {
         template.setMessageConverter(messageConverter());
         template.setConfirmCallback(((correlationData, ack, cause) -> {
 
+            if (correlationData == null) return;
+            
             Long outboxId = Long.valueOf(correlationData.getId());
             if (ack) useCase.update(outboxId, OutboxStatus.COMPLETED);
             else useCase.update(outboxId, OutboxStatus.PENDING);
