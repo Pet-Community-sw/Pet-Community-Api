5eac1dc73fb22e56d2ec3e2cf22ae8d101ceeb98
sunjaewow <chltjswo789@naver.com>
2026-03-07 15:00:36 +0900

    수정

--- src/main/java/com/example/petapp/PetAppApplication.java
+++ src/main/java/com/example/petapp/PetAppApplication.java
@@ -2,10 +2,8 @@ package com.example.petapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication(scanBasePackages = "com.example.petapp")
-@EntityScan(basePackages = "com.example.petapp")//이걸 해야지만 전체가 컴포넌트 스캔을 함 이거 이유를 좀 찾고싶음.
+@SpringBootApplication
 public class PetAppApplication {
 
     public static void main(String[] args) {
