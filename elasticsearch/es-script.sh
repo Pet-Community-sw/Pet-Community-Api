d5d647b50b602e87fcaa7a3d929adab98037e083
sunjaewow <chltjswo789@naver.com>
2026-03-12 00:15:35 +0900

    es 인덱스 생성, reindex, alias 변경 스크립트 추가

---
+++ elasticsearch/es-script.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+ES_URL="localhost:80"
+ALIAS_NAME="members"
+NEW_INDEX="member_v5"
+OLD_INDEX="member_v4"
+NEW_INDEX_FILE="elasticsearch/members/v5/indexV5.json"
+
+echo "1. 새로운 인덱스 생성 ($NEW_INDEX)"
+curl -X PUT "$ES_URL/$NEW_INDEX" \
+     -H "Content-Type: application/json" \
+     -d @"$NEW_INDEX_FILE"
+
+echo -e "\n2. 리인덱싱 시작 ($OLD_INDEX -> $NEW_INDEX) "
+curl -X POST "$ES_URL/_reindex?wait_for_completion=true" \
+     -H "Content-Type: application/json" \
+     -d @- <<EOF
+     {
+       "source": { "index": "$OLD_INDEX" },
+       "dest": { "index": "$NEW_INDEX" }
+     }
+EOF
+
+echo -e "\n3. Alias 업데이트"
+curl -X POST "$ES_URL/_aliases" \
+     -H "Content-Type: application/json" \
+     -d @- <<EOF
+     {
+       "actions": [
+         { "remove": { "index": "$OLD_INDEX", "alias": "$ALIAS_NAME" } },
+         { "add": { "index": "$NEW_INDEX", "alias": "$ALIAS_NAME" } }
+       ]
+     }
+EOF
+
+echo -e "\n4. 이전 인덱스 삭제 $OLD_INDEX"
+curl -X DELETE "$ES_URL/$OLD_INDEX"
+
+echo -e "\n5. completed"
