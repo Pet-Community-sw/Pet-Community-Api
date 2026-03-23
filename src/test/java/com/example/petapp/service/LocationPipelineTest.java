af9aef2c7f9de915a732383f8ca8f1a263c6a4b7
sunjaewow <chltjswo789@naver.com>
2026-03-23 21:06:21 +0900

    non-blocking 및 tc 수정

--- src/test/java/com/example/petapp/service/LocationPipelineTest.java
+++ src/test/java/com/example/petapp/service/LocationPipelineTest.java
@@ -6,7 +6,6 @@ import com.example.petapp.application.in.walkrecord.WalkRecordQueryUseCase;
 import com.example.petapp.application.service.location.LocationPipeline;
 import com.example.petapp.application.service.location.object.WalkRangeStatus;
 import com.example.petapp.domain.walkrecord.model.WalkRecord;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -40,7 +39,8 @@ public class LocationPipelineTest {
     @Test
     void 동일한_walkRecordId로_여러_번_send해도_초기화_한번_성공() {
         WalkRecord walkRecord = mock(WalkRecord.class);
-        when(walkRecordQueryUseCase.findOrThrow(1L)).thenReturn(walkRecord);
+
+        when(walkRecordQueryUseCase.findAndValidate(1L, 1L)).thenReturn(walkRecord);
 
         LocationMessage message1 = mock(LocationMessage.class);
         when(message1.getWalkRecordId()).thenReturn(1L);
@@ -48,12 +48,11 @@ public class LocationPipelineTest {
         LocationMessage message2 = mock(LocationMessage.class);
         when(message2.getWalkRecordId()).thenReturn(1L);
 
+        //두 번의 send 호출 - 초기화는 한 번만 수행되어야 함
         pipeline.send(message1, "1");
         pipeline.send(message2, "1");
 
-        verify(walkRecordQueryUseCase, times(1)).findOrThrow(1L);
-        verify(walkRecord, times(1)).validateMember(1L);
-        verify(walkRecord, times(1)).validateStart();
+        verify(walkRecordQueryUseCase, times(1)).findAndValidate(1L, 1L);
     }
 
     @Test
@@ -61,8 +60,8 @@ public class LocationPipelineTest {
         WalkRecord walkRecord1 = mock(WalkRecord.class);
         WalkRecord walkRecord2 = mock(WalkRecord.class);
 
-        when(walkRecordQueryUseCase.findOrThrow(1L)).thenReturn(walkRecord1);
-        when(walkRecordQueryUseCase.findOrThrow(2L)).thenReturn(walkRecord2);
+        when(walkRecordQueryUseCase.findAndValidate(1L, 1L)).thenReturn(walkRecord1);
+        when(walkRecordQueryUseCase.findAndValidate(2L, 2L)).thenReturn(walkRecord2);
 
         LocationMessage message1 = mock(LocationMessage.class);
         when(message1.getWalkRecordId()).thenReturn(1L);
@@ -73,41 +72,37 @@ public class LocationPipelineTest {
         pipeline.send(message1, "1");
         pipeline.send(message2, "2");
 
-        verify(walkRecordQueryUseCase, times(1)).findOrThrow(1L);
-        verify(walkRecordQueryUseCase, times(1)).findOrThrow(2L);
-        verify(walkRecord1, times(1)).validateMember(1L);
-        verify(walkRecord1, times(1)).validateStart();
-        verify(walkRecord2, times(1)).validateMember(2L);
-        verify(walkRecord2, times(1)).validateStart();
+        verify(walkRecordQueryUseCase, times(1)).findAndValidate(1L, 1L);
+        verify(walkRecordQueryUseCase, times(1)).findAndValidate(2L, 2L);
     }
 
     @Test
     void 초기화_실패하면_initMap에서_제거되어_다음요청때_다시_초기화_성공() {
         WalkRecord walkRecord = mock(WalkRecord.class);
-        //첫 번째 요청은 에러 두 번째 요청은 정상
-        when(walkRecordQueryUseCase.findOrThrow(1L))
+        when(walkRecord.getId()).thenReturn(1L);
+
+        // 1회차 호출 시엔 에러, 2회차엔 정상 객체 반환하도록 설정
+        when(walkRecordQueryUseCase.findAndValidate(1L, 1L))
                 .thenThrow(new RuntimeException("test error"))
                 .thenReturn(walkRecord);
 
         LocationMessage message = mock(LocationMessage.class);
         when(message.getWalkRecordId()).thenReturn(1L);
 
-        //첫 번째 호출 - 예외 발생
-        Assertions.assertThatThrownBy(() -> pipeline.send(message, "1"))
-                .hasRootCauseMessage("test error");// 루트 원인 예외 메시지 확인
-        //두 번째 호출 - 정상 처리
+        //첫 번째 호출
         pipeline.send(message, "1");
 
-        //initMap에서 제거되어 두 번 호출됨
-        verify(walkRecordQueryUseCase, times(2)).findOrThrow(1L);
-        verify(walkRecord, times(1)).validateMember(1L);
-        verify(walkRecord, times(1)).validateStart();
+        //두 번째 호출
+        pipeline.send(message, "1");
+
+        //findAndValidate 2번 호출
+        verify(walkRecordQueryUseCase, times(2)).findAndValidate(1L, 1L);
     }
 
     @Test
     void 여려번_호출해도_throttle_적용되어_2초에_한건_처리_성공() throws InterruptedException {
         WalkRecord walkRecord = mock(WalkRecord.class);
-        when(walkRecordQueryUseCase.findOrThrow(1L)).thenReturn(walkRecord);
+        when(walkRecordQueryUseCase.findAndValidate(1L, 1L)).thenReturn(walkRecord);
 
         LocationMessage message = mock(LocationMessage.class);
         when(message.getWalkRecordId()).thenReturn(1L);
