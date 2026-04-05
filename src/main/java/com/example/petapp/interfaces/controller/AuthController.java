52025de2653bcc0fde91f870e5de4a68d6c802cf
sunjaewow <chltjswo789@naver.com>
2026-04-05 17:09:05 +0900

    ci/cd test

--- src/main/java/com/example/petapp/interfaces/controller/AuthController.java
+++ src/main/java/com/example/petapp/interfaces/controller/AuthController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@Tag(name = "Auth")
+@Tag(name = "Auths")
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
