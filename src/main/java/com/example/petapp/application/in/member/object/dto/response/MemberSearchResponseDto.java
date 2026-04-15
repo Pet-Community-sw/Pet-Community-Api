2054a3d9f1ad0bd35ff172ba63fb166382255d0a
sunjaewow <chltjswo789@naver.com>
2026-04-15 20:20:26 +0900

    생성자 추가

--- src/main/java/com/example/petapp/application/in/member/object/dto/response/MemberSearchResponseDto.java
+++ src/main/java/com/example/petapp/application/in/member/object/dto/response/MemberSearchResponseDto.java
@@ -1,9 +1,11 @@
 package com.example.petapp.application.in.member.object.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 @Builder
 public class MemberSearchResponseDto {
     private Long memberId;
