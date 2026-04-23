3461bb4733238bfa99d2b587d1ef580e3263ec94
sunjaewow <chltjswo789@naver.com>
2026-04-23 20:59:37 +0900

    fix : redis 역직렬화

--- src/main/java/com/example/petapp/application/in/member/object/dto/response/MemberSearchResponseDto.java
+++ src/main/java/com/example/petapp/application/in/member/object/dto/response/MemberSearchResponseDto.java
@@ -3,9 +3,11 @@ package com.example.petapp.application.in.member.object.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class MemberSearchResponseDto {
     private Long memberId;
