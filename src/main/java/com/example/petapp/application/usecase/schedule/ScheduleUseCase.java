f6d472a4b3a44f3d1b7c0df1a98dc83e8377ecc3
sunjaewow <chltjswo789@naver.com>
2026-05-27 11:07:11 +0900

    fix : schedule관련 제거

--- src/main/java/com/example/petapp/application/usecase/schedule/ScheduleUseCase.java
+++
@@ -1,9 +0,0 @@
-package com.example.petapp.application.usecase.schedule;
-
-import com.example.petapp.domain.schedule.model.dto.response.GetSchedulesResponseDto;
-
-import java.util.List;
-
-public interface ScheduleUseCase {
-    List<GetSchedulesResponseDto> getSchedules(String start, String finish, Long profileId);
-}
