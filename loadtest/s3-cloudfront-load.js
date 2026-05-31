c6352b8e0878b02c08c51169fa79ccef6526e839
sunjaewow <chltjswo789@naver.com>
2026-05-31 10:50:40 +0900

    fix : 테스트 조건 변경

--- loadtest/s3-cloudfront-load.js
+++ loadtest/s3-cloudfront-load.js
@@ -1,14 +1,12 @@
 import http from 'k6/http';
 
-const UPLOAD_URL = __ENV.UPLOAD_URL || 'cloudfront or s3 url';
-
 export const options = {
     vus: 5,
     iterations: 100,
-    thresholds: {
-        http_req_duration: ['p(99)<1000'],
-    },
+    summaryTrendStats: ['avg', 'min', 'med', 'max', 'p(90)', 'p(95)', 'p(99)'],
 };
+
+const UPLOAD_URL = __ENV.UPLOAD_URL || 'cloudfront or s3 url';
 
 export default function () {
     http.get(UPLOAD_URL);
